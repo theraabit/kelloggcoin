996450197c5585c2e786d689a5462f7cf44c8167
theraabit <mike.raab@kellogg.northwestern.edu>
2022-02-02 02:16:25 +0000

    final

--- practice.rb
+++ practice.rb
@@ -27,3 +27,40 @@ blockchain = [
 # Anthony's KelloggCoin balance is 2650
 
 # 👇👇👇 Your code HERE 👇👇👇
+bens_balance = 0
+brians_balance = 0
+evans_balance = 0
+anthonys_balance = 0
+
+# loop through the "blockchain" and increment/decrement the balances accordingly
+for transaction in blockchain
+  # each "transaction" in the "blockchain" array is a Hash
+
+  # if the user is the "from_user", decrement their balance
+  if transaction[:from_user] == "ben"
+    bens_balance = bens_balance - transaction[:amount]
+  elsif transaction[:from_user] == "brian"
+    brians_balance = brians_balance - transaction[:amount]
+  elsif transaction[:from_user] == "evan"
+    evans_balance = evans_balance - transaction[:amount]
+  elsif transaction[:from_user] == "anthony"
+    anthonys_balance = anthonys_balance - transaction[:amount]
+  end
+
+  # if the user is the "to_user", increment their balance
+  if transaction[:to_user] == "ben"
+    bens_balance = bens_balance + transaction[:amount]
+  elsif transaction[:to_user] == "brian"
+    brians_balance = brians_balance + transaction[:amount]
+  elsif transaction[:to_user] == "evan"
+    evans_balance = evans_balance + transaction[:amount]
+  elsif transaction[:to_user] == "anthony"
+    anthonys_balance = anthonys_balance + transaction[:amount]
+  end
+end
+
+# finally, print out the result
+puts "Ben's KelloggCoin balance is #{bens_balance}"
+puts "Brian's KelloggCoin balance is #{brians_balance}"
+puts "Evan's KelloggCoin balance is #{evans_balance}"
+puts "Anthony's KelloggCoin balance is #{anthonys_balance}"
